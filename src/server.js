a29f6120546b23e9d84a96ed01c0ca330bc22ea6
pranshuteotia <pranshu28@gmail.com>
2020-12-31 18:15:19 -0500

    Changed path again

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const HEADINGS = [
 
 // app.use(cors());
 app.use(express.static(path.join(__dirname, '../../build')));
-app.get('/', (req, res, next) => res.sendFile(__dirname + '/../public/index.html'));
+app.get('/', (req, res, next) => res.sendFile(__dirname + '/index.html'));
 
 app.use("/test", (req, res) => {
 	let files = fs.readdirSync(`__dirname/../solutions`);
